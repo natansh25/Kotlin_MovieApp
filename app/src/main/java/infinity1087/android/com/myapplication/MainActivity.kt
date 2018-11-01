7751b4e95a3e2aafe1b771690fdc28e10a864869
Natansh Pandya <natansh25@gmail.com>
2018-11-01 18:33:13 -0400

    base implementation working

--- app/src/main/java/infinity1087/android/com/myapplication/MainActivity.kt
+++ app/src/main/java/infinity1087/android/com/myapplication/MainActivity.kt
@@ -2,11 +2,37 @@ package infinity1087.android.com.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyEvent
+import infinity1087.android.com.myapplication.model.ModieData
+import infinity1087.android.com.myapplication.model.Result
+import infinity1087.android.com.myapplication.service.ApiClient
+import infinity1087.android.com.myapplication.service.ApiInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //val apiService:ApiInterface = ApiClient.getClient().create(ApiInterface.class)
+
+
+        val apiServices = ApiInterface.create()
+        val call = apiServices.getPopularMovies(ApiInterface.api_key)
+
+        call.enqueue(object : Callback<ModieData> {
+            override fun onFailure(call: Call<ModieData>, t: Throwable) {
+            }
+
+            override fun onResponse(call: Call<ModieData>, response: Response<ModieData>) {
+                Log.d("xxx",response.raw().request().url().toString())
+            }
+        })
+
     }
 }
---
+++ app/src/main/java/infinity1087/android/com/myapplication/service/ApiInterface.kt
@@ -0,0 +1,37 @@
+package infinity1087.android.com.myapplication.service
+
+import infinity1087.android.com.myapplication.model.ModieData
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiInterface {
+
+
+    @GET("movie/{filter}")
+    fun getMovies(@Path("filter") filter: String, @Query("api_key") apiKey: String): Call<ModieData>
+
+    @GET("movie/popular")
+     fun getPopularMovies(@Query("api_key") apiKey: String): Call<ModieData>
+
+
+    companion object {
+        internal val MOVIE_DB_URL = "http://api.themoviedb.org/3/"
+        internal val API_KEY = "api_key"
+        val api_key = "053130b8fdf68ca19c58155b4bd37bdd"
+        fun create(): ApiInterface {
+            val retrofit = Retrofit.Builder()
+                .baseUrl(MOVIE_DB_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return retrofit.create(ApiInterface::class.java)
+        }
+    }
+
+
+
+}
+

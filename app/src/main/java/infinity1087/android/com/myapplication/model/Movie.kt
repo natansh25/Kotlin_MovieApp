4fb8363e60d259ccfa653b7ab95ab93838f3dbf1
Natansh Pandya <natansh25@gmail.com>
2018-11-01 17:06:12 -0400

    Initial commit

---
+++ app/src/main/java/infinity1087/android/com/myapplication/model/Movie.kt
@@ -0,0 +1,25 @@
+package infinity1087.android.com.myapplication.model
+
+data class ModieData(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: Any,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+)
